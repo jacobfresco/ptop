05f42b8c17a926d1615f09be3d51e35e36955b2f
Jacob Fresco <58220973+jacobfresco@users.noreply.github.com>
2024-08-08 12:10:10 +0200

    Create ptop.sh

---
+++ ptop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ptop.sh
+# Show the alerts and warnings from your PRTG server from within your terminal
+# The layout is based on the magnificient tool htop (https://github.com/htop-dev/htop)
+#
