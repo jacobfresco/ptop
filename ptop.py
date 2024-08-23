1dc56551afefedddd6fc151a2f0e27cc079d2e83
Jacob Fresco <support@jacobfresco.nl>
2024-08-23 17:39:18 +0200

    Updated code for error handling

--- ptop.py
+++ ptop.py
@@ -73,49 +73,55 @@ def main(stdscr):
 
         l = 8
         
-        if alerts == "" or warnings == "" or acks == "" or probe == "":
-            break
-
-        for i in range(int(alerts['treesize'])):
-            try:
-                draw_line(stdscr, l, 1, "", w, 12)
-                draw_value(stdscr, l, 1, "A", 12)
-                draw_value(stdscr, l, 3, alerts['sensors'][i]['objid'], 12)
-                draw_value(stdscr, l, 9, alerts['sensors'][i]['device_raw'][:21], 12)
-                draw_value(stdscr, l, 31, alerts['sensors'][i]['message_raw'][:(w-31)], 12)
-                l += 1
-            except curses.error as e:
-                logging.error("Error printing alerts: " + str(e))
-                pass
-
-        if alerts == "" or warnings == "" or acks == "" or probe == "":
-            break
+        try:
+            for i in range(int(alerts['treesize'])):
+                try:
+                    draw_line(stdscr, l, 1, "", w, 12)
+                    draw_value(stdscr, l, 1, "A", 12)
+                    draw_value(stdscr, l, 3, alerts['sensors'][i]['objid'], 12)
+                    draw_value(stdscr, l, 9, alerts['sensors'][i]['device_raw'][:21], 12)
+                    draw_value(stdscr, l, 31, alerts['sensors'][i]['message_raw'][:(w-31)], 12)
+                    l += 1
+                except curses.error as e:
+                    logging.error("Error printing alerts: " + str(e))
+                    pass
+        except curses.error as e:
+            logging.error("Error loading JSON: " + str(e))
+            pass
         
-        if settings['ptop_mode']['show_warnings'] == "true":
-            for i in range(int(warnings['treesize'])):
-                try:
-                    draw_line(stdscr, l, 1, "", w, 4)
-                    draw_value_nb(stdscr, l, 1, "W", 4)
-                    draw_value_nb(stdscr, l, 3, warnings['sensors'][i]['objid'], 4)
-                    draw_value_nb(stdscr, l, 9, warnings['sensors'][i]['device_raw'][:21], 4)
-                    draw_value_nb(stdscr, l, 31, warnings['sensors'][i]['message_raw'][:(w-31)], 4)
-                    l += 1
-                except curses.error as e:
-                    logging.error("Error printing warnings: " + str(e))
-                    pass
-            
-        if settings['ptop_mode']['show_acks'] == "true":
-            for i in range(int(acks['treesize'])):
-                try:
-                    draw_line(stdscr, l, 1, "", w, 7)
-                    draw_value_nb(stdscr, l, 1, "a", 7)
-                    draw_value_nb(stdscr, l, 3, acks['sensors'][i]['objid'], 7)
-                    draw_value_nb(stdscr, l, 9, acks['sensors'][i]['device_raw'][:21], 7)
-                    draw_value_nb(stdscr, l, 31, acks['sensors'][i]['message_raw'][:(w-31)], 7)
-                    l += 1
-                except curses.error as e:
-                    logging.error("Error printing acks: " + str(e))
-                    pass
+        try:    
+            if settings['ptop_mode']['show_warnings'] == "true":
+                for i in range(int(warnings['treesize'])):
+                    try:
+                        draw_line(stdscr, l, 1, "", w, 4)
+                        draw_value_nb(stdscr, l, 1, "W", 4)
+                        draw_value_nb(stdscr, l, 3, warnings['sensors'][i]['objid'], 4)
+                        draw_value_nb(stdscr, l, 9, warnings['sensors'][i]['device_raw'][:21], 4)
+                        draw_value_nb(stdscr, l, 31, warnings['sensors'][i]['message_raw'][:(w-31)], 4)
+                        l += 1
+                    except curses.error as e:
+                        logging.error("Error printing warnings: " + str(e))
+                        pass
+        except curses.error as e:
+            logging.error("Error loading JSON: " + str(e))
+            pass
+
+        try:
+            if settings['ptop_mode']['show_acks'] == "true":
+                for i in range(int(acks['treesize'])):
+                    try:
+                        draw_line(stdscr, l, 1, "", w, 7)
+                        draw_value_nb(stdscr, l, 1, "a", 7)
+                        draw_value_nb(stdscr, l, 3, acks['sensors'][i]['objid'], 7)
+                        draw_value_nb(stdscr, l, 9, acks['sensors'][i]['device_raw'][:21], 7)
+                        draw_value_nb(stdscr, l, 31, acks['sensors'][i]['message_raw'][:(w-31)], 7)
+                        l += 1
+                    except curses.error as e:
+                        logging.error("Error printing acks: " + str(e))
+                        pass
+        except curses.error as e:
+            logging.error("Error loading JSON: " + str(e))
+            pass
 
         try:
             draw_footer(stdscr, f"[Alerts " + str(alerts['treesize']) + "] [Warnings " + str(warnings['treesize']) +"] [Ackknowleded " + str(acks['treesize']) + "] [Q - Quit]")
